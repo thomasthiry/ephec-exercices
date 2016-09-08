2a6047f8a8e2bfeecfeca56603e23ce26f79fe8a
Thomas Thiry <thomas.thiry@ingenico.com>
2016-09-08 14:13:32 +0200

    Added exercice project with metrics

---
+++ EphecExercices.Tests/CodeCoverageTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EphecExercices.Tests
+{
+    [TestClass]
+    public class CodeCoverageTests
+    {
+        [TestMethod]
+        public void CalculateCost1()
+        {
+            var calculator = new Calculator();
+
+            var cost = calculator.CalculateCost1();
+
+            Assert.AreEqual(42, cost);
+        }
+
+        [TestMethod]
+        public void CalculateCost2()
+        {
+            var calculator = new Calculator();
+
+            var cost = calculator.CalculateCost2(500m);
+
+            Assert.AreEqual(1000, cost);
+        }
+
+        [TestMethod]
+        public void CalculateCost3()
+        {
+            var calculator = new Calculator();
+
+            var cost = calculator.CalculateCost3(2000m, 0.5m);
+
+            Assert.AreEqual(3000, cost);
+        }
+
+        [TestMethod]
+        public void CalculateCost3_2()
+        {
+            var calculator = new Calculator();
+
+            var cost = calculator.CalculateCost3(2000m, -0.5m);
+
+            Assert.AreEqual(2000, cost);
+        }
+    }
+}
---
+++ EphecExercices/Calculator.cs
@@ -0,0 +1,35 @@
+﻿namespace EphecExercices
+{
+    public class Calculator
+    {
+        public decimal CalculateCost1()
+        {
+            return 42;
+        }
+
+        public decimal CalculateCost2(decimal salary)
+        {
+            if (salary < 1000)
+            {
+                return 1000;
+            }
+            else
+            {
+                return salary;
+            }
+        }
+
+        public decimal CalculateCost3(decimal salary, decimal taxRate)
+        {
+            if (salary < 1000)
+            {
+                return 1000;
+            }
+            else
+            {
+                var taxes = salary*taxRate;
+                return salary + taxes;
+            }
+        }
+    }
+}
